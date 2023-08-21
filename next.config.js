0b905a70aa3e31486143981e21c21ff9384230c1
Guillaume Belanger <guillaume.belanger@canonical.com>
2023-08-21 11:24:12 -0400

    [Chore] Removes unneeded config file/environment variables (#58)

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
-const nextConfig = {
-  basePath: basePath,
-  assetPrefix: basePath,
-};
-
-module.exports = nextConfig;
