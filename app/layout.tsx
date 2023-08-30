c8a09b0fbcbacbcd95a2ace19a3cc82275cb59d5
guillaume <guillaume.belanger27@gmail.com>
2023-08-30 13:39:07 -0400

    Uses the theme enum

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import { Navigation } from "@canonical/react-components";
 import React, { useState, useEffect } from "react";
 import { checkBackendAvailable } from "@/utils/checkBackendAvailable";
-import { Notification } from "@canonical/react-components";
+import { Notification, Theme } from "@canonical/react-components";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navigation
-          theme="dark"
+          theme={Theme.DARK}
           logo={{
             src: "https://assets.ubuntu.com/v1/82818827-CoF_white.svg",
             title: "5G",
