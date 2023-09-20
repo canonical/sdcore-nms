9ddf19f34b97123fc960299a86715127bf8cd7cf
Guillaume Belanger <guillaume.belanger@canonical.com>
2023-09-20 15:23:28 -0400

    Re-adds subscribers code

---
+++ components/EditSubscriber.tsx
@@ -0,0 +1,109 @@
+"use client";
+import { Modal, Form, Input, Button } from "@canonical/react-components";
+import React, { ReactNode, useState, ChangeEvent } from "react";
+import { useSubscriber } from "@/hooks/useSubscriber";
+
+type Props = {
+  imsi: string;
+  currentSubscribers: string[];
+  refreshHandler: () => void;
+};
+
+export default function EditSubscriber(props: Props): ReactNode {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [opc, setOpc] = useState<string>("");
+  const [key, setKey] = useState<string>("");
+  const [sequenceNumber, setSequenceNumber] = useState<string>("");
+  const closeHandler = () => setModalOpen(false);
+  const { handleSubscriber } = useSubscriber(
+    props.imsi,
+    opc,
+    key,
+    sequenceNumber,
+    props.currentSubscribers,
+  );
+
+  const handleOpcChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setOpc(value);
+  };
+
+  const handleKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setKey(value);
+  };
+
+  const handleSequenceNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSequenceNumber(value);
+  };
+
+  const handleSave = async () => {
+    closeHandler();
+    await handleSubscriber();
+    props.refreshHandler();
+  };
+  return (
+    <>
+      <Button
+        className="u-no-margin--bottom"
+        onClick={() => setModalOpen(true)}
+        small={true}
+      >
+        Edit
+      </Button>
+      {modalOpen && (
+        <Modal
+          close={closeHandler}
+          title="Edit Subscriber"
+          buttonRow={
+            <>
+              <Button onClick={closeHandler} className="u-no-margin--bottom">
+                Close
+              </Button>
+              <Button
+                onClick={handleSave}
+                appearance="positive"
+                className="u-no-margin--bottom"
+              >
+                Save
+              </Button>
+            </>
+          }
+        >
+          <Form stacked>
+            <Input
+              type="text"
+              id="imsi"
+              label="IMSI"
+              value={props.imsi}
+              stacked
+              disabled
+            />
+            <Input
+              type="text"
+              id="opc"
+              label="OPC"
+              onChange={handleOpcChange}
+              stacked
+            />
+            <Input
+              type="text"
+              id="key"
+              label="Key"
+              onChange={handleKeyChange}
+              stacked
+            />
+            <Input
+              type="text"
+              id="sequence-number"
+              label="Sequence Number"
+              onChange={handleSequenceNumberChange}
+              stacked
+            />
+          </Form>
+        </Modal>
+      )}
+    </>
+  );
+}
---
+++ hooks/useSubscriber.tsx
@@ -0,0 +1,37 @@
+import { useCallback } from "react";
+
+export const useSubscriber = (
+  imsi: string,
+  opc: string,
+  key: string,
+  sequenceNumber: string,
+  currentSubscribers: string[],
+) => {
+  const handleSubscriber = useCallback(async () => {
+    try {
+      const response = await fetch("/api/createSubscriber", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          imsi,
+          opc,
+          key,
+          sequenceNumber,
+          currentSubscribers,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `Failed to create subscriber. Error code: ${response.status}`,
+        );
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, [imsi, opc, key, sequenceNumber, currentSubscribers]);
+
+  return { handleSubscriber };
+};
