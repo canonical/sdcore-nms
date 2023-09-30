87cec7c066ae861746f43324d95354981e14e9bc
Guillaume Belanger <guillaume.belanger@canonical.com>
2023-09-30 08:51:35 -0400

    [feat] Adds confirmation modal when deleting network slice

--- app/network-configuration/page.tsx
+++ app/network-configuration/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { Row, Col, Button, Card } from "@canonical/react-components";
+import {
+  Row,
+  Col,
+  Button,
+  Card,
+  ConfirmationModal,
+} from "@canonical/react-components";
 import { deleteNetworkSlice } from "@/utils/deleteNetworkSlice";
 import { getNetworkSlices } from "@/utils/getNetworkSlices";
 import NetworkSliceModal from "@/components/NetworkSliceModal";
@@ -10,16 +16,17 @@ import { NetworkSliceTable } from "@/components/NetworkSliceTable";
 
 export default function NetworkConfiguration() {
   const [loading, setLoading] = useState(true);
-  const [isNetworkSliceModalVisible, setisNetworkSliceModalVisible] =
-    useState(false);
   const [networkSlices, setNetworkSlices] = useState<NetworkSlice[]>([]);
-
-  const toggleNetworkSliceModal = () => {
-    setisNetworkSliceModalVisible(!isNetworkSliceModalVisible);
-  };
+  const [isNetworkSliceModalVisible, setIsNetworkSliceModalVisible] =
+    useState(false);
+  const [isDeleteNetworkSliceModalOpen, setIsDeleteNetworkSliceModalOpen] =
+    useState(false);
+  const [selectedSliceName, setSelectedSliceName] = useState<string | null>(
+    null,
+  );
 
   const fetchDataAndUpdateState = async () => {
-    const slices: NetworkSlice[] = await getNetworkSlices();
+    const slices = await getNetworkSlices();
     setNetworkSlices(slices);
     setLoading(false);
   };
@@ -28,14 +35,26 @@ export default function NetworkConfiguration() {
     fetchDataAndUpdateState();
   }, []);
 
-  const handleDeleteNetworkSlice = async (sliceName: string) => {
-    await deleteNetworkSlice(sliceName);
-    fetchDataAndUpdateState();
+  const toggleNetworkSliceModal = () =>
+    setIsNetworkSliceModalVisible((prev) => !prev);
+
+  const openDeleteConfirmationModal = (sliceName: string) => {
+    setSelectedSliceName(sliceName);
+    setIsDeleteNetworkSliceModalOpen(true);
   };
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const handleConfirmDelete = async () => {
+    if (selectedSliceName) {
+      await deleteNetworkSlice(selectedSliceName);
+      setSelectedSliceName(null);
+      setIsDeleteNetworkSliceModalOpen(false);
+      fetchDataAndUpdateState();
+    }
+  };
+
+  const closeDeleteModal = () => setIsDeleteNetworkSliceModalOpen(false);
+
+  if (loading) return <div>Loading...</div>;
 
   if (!networkSlices.length) {
     return (
@@ -49,20 +68,34 @@ export default function NetworkConfiguration() {
     <div>
       <Row>
         <Col size={6}>
+          {isDeleteNetworkSliceModalOpen && (
+            <ConfirmationModal
+              title="Confirm delete"
+              confirmButtonLabel="Delete"
+              onConfirm={handleConfirmDelete}
+              close={closeDeleteModal}
+            >
+              <p>
+                {`This will permanently delete the network slice "${selectedSliceName}".`}
+                <br />
+                You cannot undo this action.
+              </p>
+            </ConfirmationModal>
+          )}
           <h2>Network Slices</h2>
           <div className="u-align--right">
-            <Button appearance={"positive"} onClick={toggleNetworkSliceModal}>
+            <Button appearance="positive" onClick={toggleNetworkSliceModal}>
               Create
             </Button>
           </div>
           {networkSlices.map((slice) => (
             <Card key={slice.SliceName} title={slice.SliceName}>
-              {<NetworkSliceTable sliceName={slice.SliceName} />}
+              <NetworkSliceTable sliceName={slice.SliceName} />
               <hr />
               <div className="u-align--right">
                 <Button
-                  appearance={"negative"}
-                  onClick={() => handleDeleteNetworkSlice(slice.SliceName)}
+                  appearance="negative"
+                  onClick={() => openDeleteConfirmationModal(slice.SliceName)}
                 >
                   Delete
                 </Button>
