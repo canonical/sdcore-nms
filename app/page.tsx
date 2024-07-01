56a616d08c665cf7fd0efe15ce3f86fd7b1e03cb
Patricia Reinoso <patricia.reinoso@canonical.com>
2024-07-01 14:43:28 +0000

    enable redirection from home to network-configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,14 @@
-export default function Page() {}
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/network-configuration');
+  }, [router]);
+
+  return null;
+}
