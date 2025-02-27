c24fe65b1374f1e2050692109f04a57a0df5eb83
gatici <gulsum.atici@canonical.com>
2025-02-27 16:09:15 +0300

    feat: add generateOpc function
    Signed-off-by: gatici <gulsum.atici@canonical.com>

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+};
+
+export default config;
