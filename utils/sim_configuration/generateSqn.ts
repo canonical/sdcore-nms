d61c68e2e40cadb74783118d3ed27389ef204b11
gatici <gulsum.atici@canonical.com>
2025-03-05 10:16:28 +0300

    chore: address review comments
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- utils/sim_configuration/generateSqn.ts
+++ utils/sim_configuration/generateSqn.ts
@@ -1,18 +1,15 @@
 import * as crypto from "crypto";
 
 export function generateSqn(): string {
-    // 48 bytes = 6 bytes (each byte is 8 bits)
+    // 48 bits = 6 bytes (each byte is 8 bits)
     const maxBytes: number = 6;
-    // Create an array of 6 random bytes (48 bits)
     const randomBytes = new Uint8Array(maxBytes);
-    // Use the cyrpto API to fill the array with random bytes
+    // Fill the array with random bytes
     crypto.getRandomValues(randomBytes);
-    // Convert the random bytes to a hexadecimal string (12 digits)
     let sqn = Array.from(randomBytes)
         // Convert each bytes to HEX by making sure that ech number is always two hexadecimal digits
         .map(byte => byte.toString(16).padStart(2, '0'))
         .join('')
-        // Make sure that it is Uppercase
         .toUpperCase();
     return sqn;
 }
