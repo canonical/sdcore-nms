2f3f676fa672772aedb55c14d0781f66a0693442
gatici <gulsum.atici@canonical.com>
2025-03-06 10:05:56 +0300

    chore: use cyrpto.randomBytes instead of crypto.getRandomValues
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- utils/sim_configuration/generateSqn.ts
+++ utils/sim_configuration/generateSqn.ts
@@ -1,15 +1,21 @@
 import * as crypto from "crypto";
+import { OperationError } from "../errors";
 
 export function generateSqn(): string {
-    // 48 bits = 6 bytes (each byte is 8 bits)
-    const maxBytes: number = 6;
-    const randomBytes = new Uint8Array(maxBytes);
-    // Fill the array with random bytes
-    crypto.getRandomValues(randomBytes);
-    let sqn = Array.from(randomBytes)
-        // Convert each bytes to HEX by making sure that ech number is always two hexadecimal digits
-        .map(byte => byte.toString(16).padStart(2, '0'))
-        .join('')
-        .toUpperCase();
-    return sqn;
+
+    try {
+        // 48 bits = 6 bytes (each byte is 8 bits)
+        const maxBytes: number = 6;
+        const randomBytes = crypto.randomBytes(maxBytes);
+        let sqn = Array.from(randomBytes)
+            // Convert each byte to HEX by making sure that ech number is always two hexadecimal digits
+            .map(byte => byte.toString(16).padStart(2, '0'))
+            .join('')
+            .toUpperCase();
+        return sqn;
+    } catch (error) {
+        console.error(`Error generating SQN: ${error}`);
+        throw new OperationError(`SQN generation failed: ${error}`);
+    }
+
 }
