bb1660d5cdc3a944705f982e4b3031d4f62c80ce
Dario Faccin <dario.faccin@canonical.com>
2025-03-17 18:09:11 +0100

    feat: add Edit gNB modal
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

---
+++ app/(nms)/inventory/gnbs_modals.tsx
@@ -0,0 +1,154 @@
+import { Button, Form, Input, ConfirmationButton, Modal, Select } from "@canonical/react-components"
+import { editGnb } from "@/utils/gnbOperations";
+import { GnbItem } from "@/components/types";
+import { useAuth } from "@/utils/auth"
+import { useFormik } from "formik";
+import { useState } from "react"
+import { OperationError, is401UnauthorizedError}  from "@/utils/errors";
+
+import * as Yup from "yup";
+import ErrorNotification from "@/components/ErrorNotification";
+
+
+interface GnbFormValues {
+    name: string;
+    tac: number;
+}
+
+const GnbSchema = Yup.object().shape({
+    name: Yup.string()
+    .min(1)
+    .max(20, "Name must not exceed 20 characters")
+    .matches(/^[a-zA-Z][a-zA-Z0-9-_]+$/, {
+    message: (
+        <>
+        Name must start with a letter. <br />
+        Only alphanumeric characters, dashes, and underscores.
+        </>
+    ),
+    })
+    .required("Name is required."),
+    tac: Yup.number()
+        .typeError('Value should be between 1 and 16,777,215.')
+        .min(1, "Value must be greater than or equal to 1.")
+        .max(16777215, "Value must be less than or equal to 16,777,215.")
+        .required("Value should be between 1 and 16,777,215."),
+});
+
+interface GnbModalProps {
+    title: string;
+    initialValues: GnbFormValues;
+    isEdit?: boolean;
+    onSubmit: (values: any) => void;
+    closeFn: () => void
+}
+
+export const GnbModal: React.FC<GnbModalProps> = ({
+    title,
+    initialValues,
+    onSubmit,
+    closeFn,
+  }) => {
+    const auth = useAuth()
+    const [apiError, setApiError] = useState<string | null>(null);
+  
+    const formik = useFormik<GnbFormValues>({
+      initialValues,
+      validationSchema: GnbSchema,
+      onSubmit: async (values) => {
+        try {
+          onSubmit({ ...values, });
+          closeFn();
+        } catch (error) {
+          if (is401UnauthorizedError(error)) {
+              auth.logout();
+          } else if (error instanceof OperationError) {
+            setApiError(error.message);
+          } else {
+            setApiError("An unexpected error occurred.");
+          }
+        }
+      },
+    });
+
+    return (
+      <Modal
+        title={ title }
+        close={ closeFn }
+        buttonRow={
+          <>
+          <Button
+            appearance="positive"
+            onClick={formik.submitForm}
+            disabled={!(formik.isValid && formik.dirty)}
+            loading={formik.isSubmitting}
+          >
+            Submit
+          </Button>
+          <Button onClick={closeFn}>Cancel</Button>
+          </>
+        }
+      >
+        {apiError && <ErrorNotification error={apiError} />}
+        <Form>
+          <Input
+            id="name"
+            label="Name"
+            type="text"
+            required
+            stacked
+            disabled
+            placeholder="default-gnb"
+            {...formik.getFieldProps("name")}
+            error={formik.touched.name ? formik.errors.name : null}
+          />
+          <Input
+            id="tac"
+            label="TAC"
+            help="Tracking Area Code"
+            type="number"
+            required
+            stacked
+            min={1}
+            max={16777215}
+            placeholder="1"
+            {...formik.getFieldProps("tac")}
+            error={formik.touched.tac ? formik.errors.tac : null}
+          />
+        </Form>
+      </Modal>
+    );
+  };
+  
+  type editGnbModalProps = {
+    gnb: GnbItem
+    closeFn: () => void
+  }
+  
+  export function EditGnbModal({ gnb, closeFn }: editGnbModalProps) {
+    const auth = useAuth()
+    const handleSubmit = async (values: GnbFormValues) => {
+      await editGnb({
+          name: values.name,
+          tac: values.tac.toString(),
+          token: auth.user ? auth.user.authToken : ""
+        });
+    };
+  
+    const initialValues: GnbFormValues = {
+      name: gnb?.["name"] || "",
+      tac: gnb?.["tac"],
+    };
+  
+    return (
+      <>
+        <GnbModal
+          title={"Edit gNB: " + `${gnb["name"]}`}
+          initialValues={initialValues}
+          isEdit={true}
+          onSubmit={handleSubmit}
+          closeFn={closeFn}
+        />
+      </>
+    );
+  }
--- utils/gnbOperations.tsx
+++ utils/gnbOperations.tsx
@@ -1,6 +1,25 @@
 import { GnbItem } from "@/components/types";
 import { WebconsoleApiError } from "@/utils/errors";
 
+export async function apiPutGnb (name: string, gnbData: any, token: string): Promise<void>{
+  try {
+    const response = await fetch(`/config/v1/inventory/gnb/${name}`, {
+      method: "PUT",
+      headers: {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(gnbData),
+    });
+    const respData = await response.json();
+    if (!response.ok) {
+      throw new WebconsoleApiError(response.status, respData.error);
+    }
+  } catch (error) {
+    console.error(`Error in PUT gNB ${name}: ${error}`);
+    throw error;
+  }
+};
 
 export async function getGnbList(token: string): Promise<GnbItem[]> {
   try {
@@ -21,6 +40,30 @@ export async function getGnbList(token: string): Promise<GnbItem[]> {
     }));
   } catch (error) {
     console.error(`Error retrieving gNB list ${error}`);
+    throw error;
+  }
+};
+
+interface EditGnbArgs {
+  name: string;
+  tac: string;
+  token: string;
+}
+
+export async function editGnb({
+  name,
+  tac,
+  token
+}: EditGnbArgs): Promise<void>{
+
+  try {
+    const gnbData = {
+      "name": name,
+      "tac": tac,
+    };
+  await apiPutGnb(name, gnbData, token);
+  } catch (error: unknown) {
+    console.error(`Failed to edit gNB ${name} : ${error}`);
     throw error;
   }
 };
