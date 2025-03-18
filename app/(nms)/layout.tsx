081affbed0c538e84702ff9abcb871ac5d52eae8
Patricia Reinoso <patricia.reinoso@canonical.com>
2025-03-18 14:11:18 +0100

    bump react cookie

--- app/(nms)/layout.tsx
+++ app/(nms)/layout.tsx
@@ -1,13 +1,13 @@
 "use client";
 import "../globals.scss";
-import { Inter } from "next/font/google";
-import React, { useState, useEffect } from "react";
-import { List, Notification, Row } from "@canonical/react-components";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Navigation from "@/components/Navigation";
-import PageContent from "@/components/PageContent";
-import Loader from "@/components/Loader";
 import { AuthProvider, useAuth } from "@/utils/auth";
+import { CookiesProvider } from "react-cookie";
+import { Inter } from "next/font/google";
+import { List, Row } from "@canonical/react-components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import Navigation from "@/components/Navigation";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 const queryClient = new QueryClient();
@@ -21,46 +21,48 @@ export default function RootLayout({
   useAuth()
 
   return (
-    <html lang="en">
-      <head>
-        <title>Aether SD-Core</title>
-        <link
-          rel="shortcut icon"
-          href="https://assets.ubuntu.com/v1/49a1a858-favicon-32x32.png"
-          type="image/x-icon"
-        />
-      </head>
-      <body className={inter.className}>
-        <QueryClientProvider client={queryClient}>
-          <AuthProvider>
-            <div className="l-application" role="presentation">
-              <Navigation />
-              <main className="l-main">
-                <div className="p-panel">
-                  {children}
-                </div>
-                <footer className="l-footer--sticky p-strip--light">
-                  <Row>
-                    <p>
-                      © 2023 Canonical Ltd. <a href="#">Ubuntu</a> and{" "}
-                      <a href="#">Canonical</a> are registered trademarks of
-                      Canonical Ltd.
-                    </p>
-                    <List
-                      items={[
-                        <a key="Legal information" href="https://ubuntu.com/legal">
-                          Legal information
-                        </a>,
-                      ]}
-                      middot
-                    />
-                  </Row>
-                </footer>
-              </main>
-            </div>
-          </AuthProvider>
-        </QueryClientProvider>
-      </body >
-    </html >
+    <CookiesProvider>
+      <html lang="en">
+        <head>
+          <title>Aether SD-Core</title>
+          <link
+            rel="shortcut icon"
+            href="https://assets.ubuntu.com/v1/49a1a858-favicon-32x32.png"
+            type="image/x-icon"
+          />
+        </head>
+        <body className={inter.className}>
+          <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+              <div className="l-application" role="presentation">
+                <Navigation />
+                <main className="l-main">
+                  <div className="p-panel">
+                    {children}
+                  </div>
+                  <footer className="l-footer--sticky p-strip--light">
+                    <Row>
+                      <p>
+                        © 2023 Canonical Ltd. <a href="#">Ubuntu</a> and{" "}
+                        <a href="#">Canonical</a> are registered trademarks of
+                        Canonical Ltd.
+                      </p>
+                      <List
+                        items={[
+                          <a key="Legal information" href="https://ubuntu.com/legal">
+                            Legal information
+                          </a>,
+                        ]}
+                        middot
+                      />
+                    </Row>
+                  </footer>
+                </main>
+              </div>
+            </AuthProvider>
+          </QueryClientProvider>
+        </body >
+      </html >
+    </CookiesProvider>
   );
 }
